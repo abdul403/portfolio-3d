de340f2889ded7b4b67ed0fdd664cee2ae25fc39
abdul kalam <babbu36790@gmail.com>
2023-08-30 11:58:11 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+      <ToastContainer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
+
+// 2:38: 23
+// 3D models website skecthfab
