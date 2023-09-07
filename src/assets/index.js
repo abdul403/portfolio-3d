3780a7a7551c64d038760eed65e13d8683af6092
abdul kalam <babbu36790@gmail.com>
2023-09-07 13:46:50 +0530

    changes made

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,31 +4,29 @@ import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
+import vercel from "./vercel.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import nullclass from "./nullclass.jpeg";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import zustand from "./tech/zustand.jpeg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import jobfinder from "./react-native.png";
+import aisass from "./ai.png";
+import cursesio from "./cursesio.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import testimonial1 from "./testimonial/testimonial1.jpg";
+import testimonial2 from "./testimonial/testimonial2.jpg";
+import testimonial3 from "./testimonial/testimonial3.jpg";
 
 export {
   logo,
@@ -37,26 +35,24 @@ export {
   mobile,
   web,
   github,
+  vercel,
   menu,
   close,
   css,
-  docker,
-  figma,
   git,
   html,
   javascript,
   mongodb,
   nodejs,
   reactjs,
-  redux,
   tailwind,
   typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
+  jobfinder,
+  nullclass,
+  aisass,
+  cursesio,
+  testimonial1,
+  testimonial2,
+  testimonial3,
+  zustand,
 };
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { github } from "../assets";
+import { vercel } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
@@ -33,7 +34,7 @@ const ProjectCard = ({
             className="w-full h-full object-cover rounded-2xl"
           />
 
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+          <div className="absolute inset-0 gap-2  flex justify-end m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "_blank")}
               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
@@ -82,7 +83,7 @@ const Works = () => {
         >
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
+          links to code repositories. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
         </motion.p>
@@ -96,5 +97,4 @@ const Works = () => {
     </>
   );
 };
-
 export default SectionWrapper(Works, "");
